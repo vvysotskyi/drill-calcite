1f2fb1c3ad42cc9b64bd0333494aed600a4523de
Aman Sinha <asinha@maprtech.com>
2015-06-30 00:39:55 -0700

    Don't use 'SumEmptyIsZero' (SUM0) window aggregate until CALCITE-777 is fixed.

--- core/src/main/java/org/apache/calcite/sql2rel/SqlToRelConverter.java
+++ core/src/main/java/org/apache/calcite/sql2rel/SqlToRelConverter.java
@@ -4750,8 +4750,10 @@ public class SqlToRelConverter {
 
         return histogramCall;
       } else {
-        boolean needSum0 = aggOp == SqlStdOperatorTable.SUM
-            && type.isNullable();
+//        boolean needSum0 = aggOp == SqlStdOperatorTable.SUM
+//            && type.isNullable();
+        // temporarily disable SUM0 until CALCITE-777 is fixed
+        boolean needSum0 = false;
         SqlAggFunction aggOpToUse =
             needSum0 ? SqlStdOperatorTable.SUM0
                 : aggOp;
