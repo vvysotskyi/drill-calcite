vvysotskyi/calcite

[CALCITE-2018] Queries failed with AssertionError: rel has lower cost than best cost of subset. (Second attempt)

core/src/main/java/org/apache/calcite/rel/metadata/ReflectiveRelMetadataProvider.java
@@ -188,7 +188,7 @@ public class ReflectiveRelMetadataProvider
                         }
                         key = FlatLists.copyOf(args2);
                       }
-                      if (mq.map.put(key, NullSentinel.INSTANCE) != null) {
+                      if (mq.map.put(rel, key, NullSentinel.INSTANCE) != null) {
                         throw CyclicMetadataException.INSTANCE;
                       }
                       try {
@@ -198,7 +198,7 @@ public class ReflectiveRelMetadataProvider
                         Util.throwIfUnchecked(e.getCause());
                         throw new RuntimeException(e.getCause());
                       } finally {
-                        mq.map.remove(key);
+                        mq.map.remove(rel, key);
                       }
                     }
                   });

vvysotskyi/drill-calcite

(cherry picked from commit 73cb945)

core/src/main/java/org/apache/calcite/rel/metadata/ReflectiveRelMetadataProvider.java
@@ -178,7 +178,7 @@ public class ReflectiveRelMetadataProvider
                   }
                   key1 = FlatLists.copyOf(args2);
                 }
-                if (mq.map.put(key1, NullSentinel.INSTANCE) != null) {
+                if (mq.map.put(rel, key1, NullSentinel.INSTANCE) != null) {
                   throw CyclicMetadataException.INSTANCE;
                 }
                 try {
@@ -188,7 +188,7 @@ public class ReflectiveRelMetadataProvider
                   Util.throwIfUnchecked(e.getCause());
                   throw new RuntimeException(e.getCause());
                 } finally {
-                  mq.map.remove(key1);
+                  mq.map.remove(rel, key1);
                 }
               });
       methodsMap.put(key, function);